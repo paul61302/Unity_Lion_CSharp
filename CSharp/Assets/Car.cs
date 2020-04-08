b93b8dd77fe6dd9b59fa03338fdd481ae0687bda
paul61302 <paul61302@gmail.com>
2020-04-08 15:31:02 +0800

    認識欄位

--- CSharp/Assets/Car.cs
+++ CSharp/Assets/Car.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
 
 // 註解：用來說明紀錄等等.. 只會在此腳本內看到
-// 摘要 summary：描述此內容，會在其他腳本內看到，提高維護性與擴充性
+// 摘要 summary：描述此內容，會在其他腳本內看到，提高維護性與擴充性 (可讀性)
+// 新增的 C# 放在專案內為藍圖
+// 放在場景物件上為實體物件
 /// <summary>
 /// 定義汽車物件
 /// </summary>
@@ -17,13 +19,43 @@ public class Car : MonoBehaviour
     // 剎車      - 是否開啟    - 布林值 bool - ture、false
 
     // 定義欄位語法：
-    // 資料類型 欄位名稱 (指定 值);
-    // 預設值
+    // 修飾詞 資料類型 欄位名稱 (指定 值);
+
+    // 修飾詞：
+    // 公開 public ：允許其他類別存取，顯示在屬性面板(Inspector)上
+    // 私人 private (預設) ：不允許，不顯示
+
+    // 預設值：
     // int、float 為 0
     // string 為 ""
     // bool 為 false
-    int cc = 2000 ;
-    float weight = 100.9f ;
-    string brand = "BMW" ;
-    bool brake = false ;
+    // 欄位屬性 attribute：Unity 提供用於輔助欄位的功能
+    // 語法：[屬性名稱(值)]
+    // 標題 [Header("字串")]
+    // 提示 [Tooltip("字串")]
+    // 範圍 [Range(最小值,最大值)] - 僅限於數值類型資料 int、float
+
+
+    /// <summary>
+    /// 汽車的 CC 數
+    /// </summary>
+    [Header("汽車的 CC 數")]
+    [Range(1000,10000)]
+    public int cc = 2000 ;
+    /// <summary>
+    /// 汽車的重量
+    /// </summary>
+    [Header("汽車的重量")]
+    [Tooltip("今天很重是吧")]
+    public float weight = 100.9f ;
+    /// <summary>
+    /// 汽車的品牌
+    /// </summary>
+    [Header("汽車的品牌")]
+    public string brand = "BMW" ;
+    /// <summary>
+    /// 汽車有沒有剎車
+    /// </summary>
+    [Header("汽車有沒有剎車")]
+    public bool brake = false ;
 }
--- CSharp/Assets/Class1.cs
+++ CSharp/Assets/Class1.cs
@@ -1,15 +1,26 @@
 ﻿using UnityEngine;    // 引用 Unity 的API (函式庫：Unity 屬性、功能...)
 
+
+
 // 類別 Class 定義一個物件
 // 語法：class 類別名稱
 // ：MonoBehaviour 可以將此腳本掛在 Unity 物件上
 // 類別必須放在物件上才會執行
 public class Class1 : MonoBehaviour
 {
-    // 定義類別成員
+        // 定義類別成員
     // C#
     // 1. 英文字母大小寫不一樣
     // 2. 括號一定成對出現 () [] {} ' ' " " < >
+
+    [Header("汽車1號")]
+    public Car car1;
+
+    [Header("汽車2號")]
+    public Car car2;
+
+    [Header("汽車3號")]
+    public Car car3;
 
     // 事件：在特定時間會以指定次數執行的方法 (名稱為藍色)
     // 開始事件：撥放遊戲時執行一次 - 初始設定
@@ -18,5 +29,16 @@ public class Class1 : MonoBehaviour
         // 輸出("文字訊息")
         //C# 結尾為分號 ;
         print("ㄩㄇ");
+
+        // 取得 get
+        print("汽車1號的 CC 數" + car1.cc);
+        print("汽車1號的品牌" + car2.brand);
+
+        // 設定 set
+        car1.cc = 9999;
+        car1.weight = 2.3f;
+        car1.brand = "Tesla";
+        car1.brake = true;
+
     }
 }
