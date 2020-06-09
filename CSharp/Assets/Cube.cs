c1e91938aa5793114a4a8342639ce118c8ef2ecb
paul61302 <paul61302@gmail.com>
2020-06-09 15:43:13 +0800

    123

---
+++ CSharp/Assets/Cube.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 立方體 繼承 形狀類別
+/// 繼承可擁有父類別的公開成員
+/// 可存取公開成員
+/// </summary>
+public class Cube : Shape
+{
+    private void Start()
+    {
+        print(gameObject + "體積：" + GetVolume());
+    }
+}
---
+++ CSharp/Assets/Shape.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 形狀：基底類別
+/// </summary>
+public class Shape : MonoBehaviour
+{
+    [Header("長寬高")]
+    public float length;
+    public float width;
+    public float height;
+
+    /// <summary>
+    /// 體積
+    /// protected 保護：允許子類別存取，但禁止外部存取
+    /// </summary>
+    protected float volume;
+
+    /// <summary>
+    /// 取得體積
+    /// </summary>
+    /// <returns>體積</returns>
+    public virtual float GetVolume()
+    {
+        volume = length * width * height;
+        return volume;
+    }
+
+}
