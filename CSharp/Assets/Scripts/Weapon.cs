2d83f893bacc576933258bccb3c5ad05a1bc11a4
paul61302 <paul61302@gmail.com>
2020-06-29 13:24:35 +0800

    123

---
+++ CSharp/Assets/Scripts/Weapon.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace KID.Class8
+{
+    public class Weapon : MonoBehaviour, ICanUse
+    {
+        public Data data;
+
+        public void Use()
+        {
+            print("使用武器：" + data.name);
+
+            transform.SetParent(FindObjectOfType<Player>().hand);       // 設定父物件(玩家.手)
+            transform.localEulerAngles = new Vector3(70, 0, 0);         // 旋轉角度
+            transform.localPosition = new Vector3(0, 0, 40);
+            //transform.localPosition = Vector3.zero;                     // 區域座標 = 零
+            GetComponent<Collider>().enabled = false;                   // 關閉碰撞
+        }
+    }
+}
