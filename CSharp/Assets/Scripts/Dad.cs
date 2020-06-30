33c96be7dde0dc75e269f57c2e1e56d4983ce7e5
paul61302 <paul61302@gmail.com>
2020-06-30 16:26:50 +0800

    123

---
+++ CSharp/Assets/Scripts/Dad.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace KID.Class9
+{
+    public class Dad : MonoBehaviour
+    {
+        public Boy boy;
+        
+        void Start()
+        {
+            boy.onNoMoney += HitBoy;
+            boy.onNoMoney += GiveMoney;
+        }
+
+        private void GiveMoney()
+        {
+            boy.money += 10;
+            print("10塊錢");
+        }
+
+        private void HitBoy()
+        {
+            print("挖嘎挖嘎");
+        }
+    }
+}
+
---
+++ CSharp/Assets/Scripts/Mom.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace KID.Class9
+{
+    public class Mom : MonoBehaviour
+    {
+        // 訂閱者：訂閱事件
+        public Boy boy;
+
+        private void Start()
+        {
+            // 1. 訂閱事件
+            boy.onNoMoney += GiveMoney;
+        }
+
+        // 2. 實作方法
+        private void GiveMoney()
+        {
+            print("= =");
+            boy.money += 1000;
+        }
+    }
+}
+
